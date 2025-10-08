51df44107a9d37915774cc0b58d5e8e64ad4b679
Rimon <rimon@Rimons-MacBook-Air.local>
2025-10-08 22:10:41 +0600

    Initial commit

---
+++ codeforces/watermilon.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int weight;
+    cin >> weight;
+    
+   if (weight>2)
+   {
+     if (weight %2==0)
+    {
+        cout<<"YES"<<endl;
+    }
+   else{
+        cout<<"NO"<<endl;
+    }
+    
+   }
+   else{
+    cout<<"NO"<<endl;
+   }
+   
+    
+
+    return 0;
+}
